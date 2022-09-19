03878207c1975a3fc5ec056e102e1673cd942e99
Aasupa <aasupadbbaral@gmail.com>
2022-09-19 16:26:15 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu= document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-menu").forEach(n => n.
+    addEventListener("click",() => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+
+
+    var swiper = new Swiper(".home-slider", {
+        loop:true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+     });
---
+++ service.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+
+<section class="header">
+    <a href="index.php" class="logo">GhumGham.</a>
+    <nav class="navbar">
+        <div class="nav-menu">
+        <a href="index.php">Home</a>
+        <a class="active" href="service.php">Service</a>
+        <a href="package.php">Packages</a>
+        <a href="book.php">Book</a>
+        <a href="login.php">Signup</a>
+        </div>
+
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+
+    </nav>
+    
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="./js/script.js"></script>
+</body>
+</html>
