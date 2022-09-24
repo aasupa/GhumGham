91fa4b331becfe59fd9e66c957f0da6ffccab409
Aasupa <aasupadbbaral@gmail.com>
2022-09-24 15:31:32 +0545

    contact

--- js/jscript.js
+++ js/jscript.js
@@ -15,20 +15,22 @@ document.querySelectorAll(".nav-menu").forEach(n => n.
 
 
 
-    var swiper = new Swiper(".home-slider", {
-        loop:true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-     });
 
-     
+var swiper = new Swiper(".home-slider", {
+      loop:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+   });
+
+
+   
 
 let loadMoreBtn = document.querySelector('.packages .load-more .btn');
 let currentItem = 3;
 
-loadMoreBtn.onclick = () => {
+loadMoreBtn.onclick = () =>{
    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
    for (var i = currentItem; i < currentItem + 3; i++){
       boxes[i].style.display = 'inline-block';
